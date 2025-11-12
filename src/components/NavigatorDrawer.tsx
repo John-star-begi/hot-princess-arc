80dce6cb9091468e08e0eba16cfd0bc9dc1ca9c4
John-star-begi <John@classafix.com.au>
2025-11-12 15:42:33 +1100

    Update NavigatorDrawer.tsx

--- src/components/NavigatorDrawer.tsx
+++ src/components/NavigatorDrawer.tsx
@@ -110,7 +110,7 @@ export default function NavigatorDrawer() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5 text-rose-700"
+            className="w-8 h-8 text-rose-700"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
